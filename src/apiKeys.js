65e763d95b930c58d477661a5234f94cf1d75afe
Shubham Abhisek Dash <100282638+Cypher-Knight@users.noreply.github.com>
2024-03-05 13:09:14 +0530

    first commit

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "2b544c82fce0abb907715f605d9296d3",
+  key: "[Enter Your API Key]",
   base: "https://api.openweathermap.org/data/2.5/",
 };
