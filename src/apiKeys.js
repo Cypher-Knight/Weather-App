bc756e2792953c54786591160aa6786c87595278
Shubham Abhisek Dash <shubhamabhisekdash2002@gmail.com>
2024-03-05 12:35:56 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "2b544c82fce0abb907715f605d9296d3",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
